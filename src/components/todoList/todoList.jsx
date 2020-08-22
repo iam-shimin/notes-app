3de3b211185c2ce00e1312de32f2556e710ccc43
shimin mathew <simplyshimin@gmail.com>
2020-08-22 12:45:39 +0530

    redirect to todo creation page if no todos exist in store on first load

--- src/components/todoList/todoList.jsx
+++ src/components/todoList/todoList.jsx
@@ -1,11 +1,20 @@
 import React, { /* createContext */ } from 'react';
 import { Route, Switch, Redirect } from 'react-router';
+import { connect } from 'react-redux';
 
 import SideNav from 'components/sideNav';
 import Todo from './Todo';
 import RecentTodo from './recentTodo';
 import NewTodo from './newTodo';
 import MostVisited from './mostVisited';
+
+function TodoListRedirect({hasTodos}) {
+	const redirectionURL = hasTodos ? '/notes/recent': '/notes/new';
+	return <Redirect to={redirectionURL} />;
+}
+
+const mapStateToProps = state => {hasTodos: state.todos.length !== 0}
+const LandingTodoRedirect = connect(mapStateToProps)(TodoListRedirect);
 
 export default function TodoList(props) {
 	return (
@@ -17,7 +26,7 @@ export default function TodoList(props) {
 					<Route path="/notes/most-checked" component={MostVisited} />
 					<Route path="/notes/new" component={NewTodo} />
 					<Route path="/notes/:id" component={Todo} />
-					<Redirect to="/notes/recent" />
+					<LandingTodoRedirect />
 				</Switch>
 			</article>
 		</React.Fragment>
