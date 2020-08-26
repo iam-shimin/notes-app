be1bc4744eaf0fd69eda392f7d7be2823e0544a6
shimin mathew <simplyshimin@gmail.com>
2020-08-26 22:09:00 +0530

    test sidenavitems

---
+++ src/components/sideNav/__test__/sideNavItems.test.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import {MemoryRouter, Router} from 'react-router';
+import {createMemoryHistory} from 'history';
+
+import {SideNavItems} from '../sideNavItems';
+
+describe('SideNavItems', () => {
+	const sample = [
+		{id: 1, title: 'app', notes: 'verthe'},
+		{id: 2, title: 'bbb', notes: 'lorem'},
+		{id: 3, title: 'application', notes: 'this is fine'}
+	];
+
+	test('render items', () => {
+		const {getAllByRole} = renderItem(sample);
+		const items = getAllByRole('link');
+
+		expect(items.length).toBe(sample.length);
+	})
+
+	test('search filter works', () => {
+		const history = createMemoryHistory();
+		history.push('?q=app');
+		const {getAllByRole} = render(
+			<Router history={history}>
+				<SideNavItems data={sample} />
+			</Router>
+		);
+		const items = getAllByRole('link');
+
+		expect(items.length).toBe(2);
+	})
+
+	test('right clicking on item will select it', () => {
+		const onContextMenu = jest.fn();
+		const {getAllByRole} = renderItem(sample, {onContextMenu});
+		const [firstLink] = getAllByRole('link');
+		fireEvent.contextMenu(firstLink);
+
+		expect(firstLink).toHaveClass('selected');
+		expect(onContextMenu).toHaveBeenCalledWith(sample[0].id);
+	})
+
+	test('clicking on a selected item will unselect it', () => {
+		let isSelectionModeOn = false;
+		const onContextMenu = jest.fn(() => isSelectionModeOn = !isSelectionModeOn);
+		const onClick = jest.fn();
+		const {getAllByRole} = renderItem(sample, {onContextMenu, onClick, isSelectionModeOn});
+		const [firstLink] = getAllByRole('link');
+		fireEvent.contextMenu(firstLink);
+		fireEvent.click(firstLink);
+
+		expect(firstLink).not.toHaveClass('selected');
+		expect(onContextMenu).toHaveBeenCalledWith(sample[0].id);
+		// TODO: expected 2, learn rerender anf fix this
+		expect(onContextMenu).toHaveBeenCalled();
+		expect(onClick).toHaveBeenCalledTimes(1);
+	})
+
+	test('right clicking on a selected item will unselect it', () => {
+		// TODO: fix this variable, it does not cause automatic rerender, trying to gives error
+		let isSelectionModeOn = false;
+		const onContextMenu = jest.fn(() => isSelectionModeOn = !isSelectionModeOn);
+		const onClick = jest.fn();
+		const {getAllByRole} = renderItem(sample, {onContextMenu, onClick, isSelectionModeOn});
+		const [firstLink] = getAllByRole('link');
+		fireEvent.contextMenu(firstLink);
+		fireEvent.contextMenu(firstLink);
+
+		expect(firstLink).not.toHaveClass('selected');
+		expect(onContextMenu).toHaveBeenCalledWith(sample[0].id);
+		expect(onContextMenu).toHaveBeenNthCalledWith(2, sample[0].id);
+	})
+});
+
+function renderItem(data, {isSelectionModeOn, onContextMenu, onClick} = {}) {
+	return render(
+		<MemoryRouter>
+			<SideNavItems
+				data={data}
+				isSelectionModeOn={isSelectionModeOn}
+				onContextMenu={onContextMenu}
+				onClick={onClick} />
+		</MemoryRouter>
+	)
+}
