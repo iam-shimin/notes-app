00e1f88ced4734089d0092c660f72da3b0600357
shimin mathew <simplyshimin@gmail.com>
2020-12-03 00:23:33 +0530

    refactor: todo reducer dynamic test names

--- src/reducers/todoReducer.test.js
+++ src/reducers/todoReducer.test.js
@@ -10,15 +10,23 @@ const payloads = {
 	[SET_TODO_FIELD]: { id: todo.id, field: 'notes', value: '...'}
 }
 
+const expectations = {
+	[CREATE_TODO]: [...sampleTodos, todo ],
+	[DELETE_TODO]: sampleTodos.filter(t => t.id !== todo.id),
+	[SET_TODO_FIELD]: sampleTodos.map(t => t.id === todo.id? { ...todo, notes: '...' }: t)
+}
+
 describe('todo reducer', () => {
 	test.each([
 		[CREATE_TODO],
 		[DELETE_TODO],
 		[SET_TODO_FIELD]
-	])('action', (actionType) => {
-		reducer(sampleTodos, {
+	])('%s action', (actionType) => {
+		const state = reducer(sampleTodos, {
 			type: actionType,
 			payload: payloads[actionType]
-		})
+		});
+
+		expect(state).toStrictEqual(expect.arrayContaining(expectations[actionType]));
 	})
 })
