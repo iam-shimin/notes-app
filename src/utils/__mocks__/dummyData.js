5f6ae02e3b8bbf0a2c5783ce2cb2510657847da7
shimin mathew <simplyshimin@gmail.com>
2020-12-02 22:33:57 +0530

    refactor: extract dummy data out of storage mock

---
+++ src/utils/__mocks__/dummyData.js
@@ -0,0 +1,14 @@
+export const sampleVisitFrequency = { 1597731883759: 75, 1599332969029: 7 };
+
+export const sampleTodos = [{
+	id: 1597731883759,
+	notes: "",
+	priority: "med",
+	title: "code FE"
+}, {
+	id: 1599332969029,
+	notes: "",
+	title: "applemnbn"
+}];
+
+export const expectedResult = { 1597731883759: 75, 1599332969029: 8 };
--- src/utils/__mocks__/storage.js
+++ src/utils/__mocks__/storage.js
@@ -1,10 +1,14 @@
+import { sampleVisitFrequency, sampleTodos } from './dummyData';
+
 // NOTES
-export const loadTodos = jest.fn()
+export const loadTodos = jest.fn().mockReturnValue(sampleTodos);
 
 export const persistTodos = jest.fn(currentStateTodos => {})
 
 
 // VISIT COUNTERS
+const getVisitFrequency = jest.fn().mockReturnValue(sampleVisitFrequency);
+
 export const initVisitCounters = jest.fn()
 
 export const increaseCount = jest.fn(noteId => {})
