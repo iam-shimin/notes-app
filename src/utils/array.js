a8a7bcb051114c40f1a44058b1ab6af472265342
shimin mathew <simplyshimin@gmail.com>
2020-12-17 16:28:50 +0530

    chore: add jsdocs to array utils

--- src/utils/array.js
+++ src/utils/array.js
@@ -1,3 +1,8 @@
+/**
+ * Return the last item of the Arraylike
+ * @param array - the array
+ * @returns lastItems
+ **/
 export default function lastOf(array) {
 	if (typeof array.length !== 'number') return null;
 
