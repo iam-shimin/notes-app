25292dd3b2c7cbe6e42872b26ba68675f2fd6b59
shimin mathew <simplyshimin@gmail.com>
2021-01-02 11:24:33 +0530

    fix: storage breaking on first app load

--- src/store/persistanceMiddleware.js
+++ src/store/persistanceMiddleware.js
@@ -3,7 +3,7 @@ import { persistNotes } from 'utils/storage';
 export default function persistanceMiddleware(store) {
 	return next => action => {
 		const res = next(action);
-		persistNotes(store.getState().todos);
+		persistNotes(store.getState().notes);
 		return res;
 	};
 }
