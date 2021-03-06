490c4bce8a6bfd35f805ec6598a7fb272bff0155
shimin mathew <simplyshimin@gmail.com>
2021-03-06 18:11:51 +0530

    style: move the toast so that it will not mess the floating button

--- src/components/notifications/notifications.tsx
+++ src/components/notifications/notifications.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Toast from './Toast';
+import useMatchMedia from 'hooks/useMatchMedia';
 
 interface NotificationsProps {
 	notifications: NotificationI[],
@@ -7,10 +8,11 @@ interface NotificationsProps {
 };
 
 export default function Notifications({ notifications, dismissToast }: NotificationsProps) {
+	const isMobile = useMatchMedia('(max-width: 500px)');
 	const activeCssClass = notifications.length > 0 ? ' active' : '';
 
 	return (
-		<div className="notifications">
+		<div className="notifications" style={{right: isMobile? '21%': '5%'}}>
 			<div className={`notifications-toasts${activeCssClass}`}>
 				{notifications.map(msg => (
 					<Toast key={msg._id} messageObj={msg} onDismiss={dismissToast} />
